2558b7c4c2a02b179cf487a8c79233564da7b615
Nik Alexoff <nik.alexoff@gmail.com>
2023-03-25 12:06:53 -0400

    new file for getting buoy data

---
+++ buoys.py
@@ -0,0 +1,8 @@
+# problem: need to retrieve buoy data from the web
+# identify sources of buoy data relevant to me  
+# identify refresh rate of the data
+# determine how to retrieve the data
+# determine how to store the data - suggested by AP
+# determine how to use the data - suggested by AP
+
+
