9805aa2be339c08259a9712b727c7a5e92ef3d3f
Nik Alexoff <nik.alexoff@gmail.com>
2023-04-08 10:06:29 -0400

    extracting text update

--- surf-captain.py
+++ surf-captain.py
@@ -17,6 +17,9 @@ driver.get(url)
 # Find all the elements with the class name "summary-day-expand"
 buttons = driver.find_elements(By.CLASS_NAME, "summary-day-expand")
 
+# Initialize an empty list to store the extracted text after each button interaction
+extracted_text_list = []
+
 # Iterate through the first three buttons in the list of found elements and interact with them individually
 for i, button in enumerate(buttons[:3]):
     # Perform your desired action with the button
@@ -26,7 +29,18 @@ for i, button in enumerate(buttons[:3]):
     # Add wait time between button interactions
     time.sleep(3)  # Adjust the sleep time based on your requirements
 
-    # Add any other interaction or data extraction code here
+    # Add the code to extract the text from the newly displayed information
+    text_element = driver.find_element(By.CSS_SELECTOR, ".large-6.day-summary-text.day-summary-surf")
+
+    # Get the text from the element and store it in the list
+    extracted_text = text_element.text
+    extracted_text_list.append(extracted_text)
+
+    # Print the extracted text for debugging purposes
+    print(f"Extracted text after interacting with button {i + 1}: {extracted_text}")
 
 # Close the driver
 driver.quit()
+
+# You can now use the extracted_text_list variable to work with the extracted text
+print("All extracted text:", extracted_text_list)
